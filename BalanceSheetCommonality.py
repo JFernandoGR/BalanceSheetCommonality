8a07e7c61e5e257d5f386a9e6ddb27df962bacad
Jairo F. Gudiño R <JFernandoGR@users.noreply.github.com>
2018-01-21 16:24:44 -0500

    Update BalanceSheetCommonality.py

--- BalanceSheetCommonality.py
+++ BalanceSheetCommonality.py
@@ -3,38 +3,76 @@
 Created on Sat Dec 16 20:20:03 2017
 
 @author: JairoFGR
+
 """
+
 # Import libraries #
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 import numpy as np
-from sklearn import metrics
-from sklearn.metrics.pairwise import euclidean_distances
 import seaborn as sns; sns.set()
 import matplotlib.pyplot as plt
-from sklearn.cluster import DBSCAN
+from sklearn.neighbors import DistanceMetric
+# https://github.com/nicodv/kmodes --> K-prototypes
+import pip
+pip.main(['install','mca'])
+import mca
+
+def gower_distance(X):
+    """
+    This function expects a pandas dataframe as input
+    The data frame is to contain the features along the columns. Based on these features a
+    distance matrix will be returned which will contain the pairwise gower distance between the rows
+    All variables of object type will be treated as nominal variables and the others will be treated as 
+    numeric variables.
+    Distance metrics used for:
+    Nominal variables: Dice distance (https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient)
+    Numeric variables: Manhattan distance normalized by the range of the variable (https://en.wikipedia.org/wiki/Taxicab_geometry)
+    """
+    individual_variable_distances = []
+    for i in range(X.shape[1]):
+        feature = X.iloc[:,[i]]
+        if feature.dtypes == np.float: #Se revisa si el feature es un float o no. Modificación realizada.
+            feature_dist = DistanceMetric.get_metric('manhattan').pairwise(feature) / np.ptp(feature.values)
+        else:
+            feature_dist = DistanceMetric.get_metric('dice').pairwise(pd.get_dummies(feature))
+        individual_variable_distances.append(feature_dist)
+    return np.array(individual_variable_distances).mean(0)
 
 # Read Database: #
 xl = pd.ExcelFile('C:/Users/Jairo F Gudiño R/Desktop/Balance Sheet Commonality/FinalDatabase2.xlsx')
+# NUMERICAL VARIABLES #
 # Read features information
 df = xl.parse(xl.sheet_names[0])
 BalanceSheetNames = list(df)[1:]
 df = df.fillna(0) #Fill NaNs with zeros
 dx = pd.DataFrame(df, columns = BalanceSheetNames)
+rows,columns = df.shape
 # Descriptive Statistics by Feature
 descriptivestats = dx.describe()
 correlationMatrix = dx.corr() #Correlation Matrix
 covarianceMatrix = df.cov() #Covariance Matrix
+# Standarization of Numerical Variables
+df_norm = pd.DataFrame(StandardScaler().fit_transform(df))
+# CATEGORICAL VARIABLES #
 # Read Industry Type Information
 di = xl.parse(xl.sheet_names[1])
 IndustryO = di.iloc[:,2].str.split(' ', expand=True).iloc[:,0].str[0]
-# Read Industry Classes & Replace Categories by Numbers
+Departments = di.iloc[:,5]
+City = di.iloc[:,6].str.split('-', expand=True)
+NaNCells = pd.concat([pd.Series(list(range(rows))),5 - City.isnull().sum(axis=1)], axis=1)
+di.iloc[:,2].str.split(' ', expand=True)
+# Read industry classes to get info about categories
 dw = xl.parse(xl.sheet_names[3])
 industriesnumber = len(dw['Letter'])
-# Standarization of Variables
-df_norm = pd.DataFrame(StandardScaler().fit_transform(df))
+
 
 # Descriptive Analysis #
+# Multiple-Correspondence Analysis #: COMPLETAR
+mca_ben = mca.MCA(df, ncols=39)
+print(mca_ben.fs_r(1))
+print(mca_ben.L)
+# http://nbviewer.jupyter.org/github/esafak/mca/blob/master/docs/mca-BurgundiesExample.ipynb
 # Mean Values by Feature of Industry
 dx['Industry'] = pd.DataFrame(IndustryO)
 MeanbyVariable = dx.groupby('Industry').mean()
@@ -53,6 +91,7 @@ for element in range(0,industriesnumber):
     RearrangedRows[0:len(Indexes)] = Indexes
  df_norm_i = df_norm.iloc[Indexes,:]
 # General Features by Industry #
+# Add Distances by City, Department # ENDOGENEIDAD?
  with sns.axes_style("white"):
   fig, Graph = plt.subplots()
   Graph = sns.heatmap(df_norm_i, xticklabels=False, yticklabels=False)
@@ -60,33 +99,33 @@ for element in range(0,industriesnumber):
   fig.savefig(''.join(['C:/Users/Jairo F Gudiño R/Desktop/Balance Sheet Commonality/',dw['Name'][element],'_GeneralFeatures.pdf']))
  rows,columns = df_norm_i.shape
  if rows!=0:
-  EuDist  = euclidean_distances(df_norm_i, df_norm_i)
-  Distance[0][element] = np.triu(EuDist, k=0).sum() / rows
-  mask = np.zeros_like(EuDist)
+# INCLUIR ESCALA DE CONTRIBUCIÓN EN LOS GRÁFICOS #
+  GowerDist  = gower_distance(df_norm_i)
+  Distance[0][element] = np.triu(GowerDist, k=0).sum() / rows
+  mask = np.zeros_like(GowerDist)
   mask[np.triu_indices_from(mask)] = True
  with sns.axes_style("white"):
   fig, Graph = plt.subplots()
-  Graph = sns.heatmap(EuDist, mask=mask, square=True, xticklabels=False, yticklabels=False)
+  Graph = sns.heatmap(GowerDist, mask=mask, square=True, xticklabels=False, yticklabels=False)
   Graph.set_title(''.join([dw['Name'][element]]))      
   fig.savefig(''.join(['C:/Users/Jairo F Gudiño R/Desktop/Balance Sheet Commonality/',dw['Name'][element],'.pdf']))
 
 # General Distance Matrix #
 df_norm_total = df_norm.reindex(RearrangedRows)
-EuDist_total  = euclidean_distances(df_norm_total, df_norm_total)
-mask = np.zeros_like(EuDist_total)
+# INCLUIR ESCALA DE CONTRIBUCIÓN EN LOS GRÁFICOS #
+GowerDist_total  = gower_distance(df_norm_total)
+mask = np.zeros_like(GowerDist_total)
 mask[np.triu_indices_from(mask)] = True
 with sns.axes_style("white"):
   fig, Graph = plt.subplots()
-  Graph = sns.heatmap(EuDist_total, mask=mask, square=True, xticklabels=False, yticklabels=False)
+  Graph = sns.heatmap(GowerDist_total, mask=mask, square=True, xticklabels=False, yticklabels=False)
   Graph.set_title('Distance Matrix for All Enterprises')      
   fig.savefig(''.join(['C:/Users/Jairo F Gudiño R/Desktop/Balance Sheet Commonality/','TotalDistance','.pdf']))
 
-# Estimation #
-db = DBSCAN(eps=20, min_samples=1).fit(df_norm)
-core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
-core_samples_mask[db.core_sample_indices_] = True
-labels = db.labels_
-n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-print('Estimated number of clusters: %d' % n_clusters_)
-print("Calinski-Harabasz Coefficient: %0.3f"
-      % metrics.calinski_harabaz_score(df_norm, labels))
+# Estimation: K-Prototypes #
+
+# Testing Parameters #
+Calinski_Harabasz = np.zeros(dtype=np.int, shape=100)
+for element in range(2, 100):
+     Calinski_Harabasz[element] = metrics.calinski_harabaz_score(df_norm, kmeans_model.labels_)
+
